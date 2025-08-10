7c9b273be34b087133dd96a43969a602202bdcfa
buttonz13 <buttonz13@gmail.com>
2025-08-09 20:08:32 -0700

    Add files via upload

--- bingo_bot.py
+++ bingo_bot.py
@@ -1,17 +1,3 @@
-from flask import Flask
-import threading
-
-app = Flask('')
-
-@app.route('/')
-def home():
-	return "Bot is running!"
-
-def run():
-	app.run(host='0.0.0.0', port=8080)
-
-threading.Thread(target=run).start()
-
 import discord
 from discord.ext import commands
 import random
